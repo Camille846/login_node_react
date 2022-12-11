a19136f6cc01d0199163cb2f2d46208d35ecb568
Camille <83260908+Camille846@users.noreply.github.com>
2022-12-10 21:36:44 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginPage from './assets/Components/LoginPages/login'
+function App() {
+
+  return (
+    <div className="App">
+      <LoginPage />
+      {/* <HomePage /> */}
+    </div>
+  )
+}
+
+export default App
